ab1ec3ff827372fa6b520d3779997503b0e745cb
Xander Warszawski <xander@xdoubleu.com>
2023-03-31 17:43:00 +0200

    WIP deploy

--- apps/api/src/sse/sse.controller.ts
+++ apps/api/src/sse/sse.controller.ts
@@ -15,12 +15,16 @@ export class SseController {
   @Sse()
   @Header("Access-Control-Allow-Origin", "*")
   @Header("Content-Type", "text/event-stream")
-  @Header("Cache-Control", "no-cache, no-store")
+  @Header("Cache-Control", "no-cache, no-transform")
+  @Header("Connection", "keep-alive")
   public sseAllLocations(): Observable<LocationUpdateEvent> {
     return this.sseService.sendAllLocationUpdates()
   }
 
   @Sse(":normalizedName")
+  @Header("Content-Type", "text/event-stream")
+  @Header("Cache-Control", "no-cache, no-transform")
+  @Header("Connection", "keep-alive")
   public sseSingleLocation(
     @Param("normalizedName") normalizedName: string
   ): Observable<LocationUpdateEvent> {
