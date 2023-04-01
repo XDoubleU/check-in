027237791f3239841116fd4c33e9f8f37651a0d5
Xander Warszawski <xander@xdoubleu.com>
2023-04-01 18:52:57 +0200

    WIP deploy

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -2,7 +2,8 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs"
+import { Replay } from "@sentry/browser"
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
@@ -17,5 +18,5 @@ Sentry.init({
 
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
-  integrations: [new Sentry.Replay()]
+  integrations: [new Replay()]
 });
