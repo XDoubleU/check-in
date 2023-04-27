9e908cd44a6349cfc050a0b11a415cd4c81f6d13
Xander Warszawski <xander@xdoubleu.com>
2023-04-27 14:21:57 +0200

    Fixed code quality issue

--- apps/api/src/helpers/normalization.ts
+++ apps/api/src/helpers/normalization.ts
@@ -2,5 +2,5 @@ export function normalizeName(name: string): string {
   return name
     .toLowerCase()
     .replaceAll(/\s/g, "-")
-    .replaceAll(/^-+|[^a-z0-9-]|-+$/g, "")
+    .replaceAll(/^-+|[^a-z0-9-]|(?<!-)-+$/g, "")
 }
