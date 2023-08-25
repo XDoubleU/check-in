e0c79462af37e28581098fe57da941672221cb06
Xander Warszawski <xander@xdoubleu.com>
2023-08-25 13:18:14 +0200

    Added more fun error page (#405)

--- web/src/layouts/Error.tsx
+++ web/src/layouts/Error.tsx
@@ -1,12 +1,26 @@
+import styles from "./error.module.css"
 import Head from "next/head"
+import { type MouseEventHandler, useRef } from "react"
 
 export default function Error() {
-  const style: React.CSSProperties = {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    textAlign: "center"
+  const leftEye = useRef<HTMLDivElement>(null)
+  const rightEye = useRef<HTMLDivElement>(null)
+
+  const moveEyes: MouseEventHandler<HTMLDivElement> = (event) => {
+    if (!leftEye.current || !rightEye.current) return
+
+    const lx = leftEye.current.offsetLeft + leftEye.current.offsetWidth / 2
+    const ly = leftEye.current.offsetTop + leftEye.current.offsetHeight / 2
+    const lrad = Math.atan2(event.pageX - lx, event.pageY - ly)
+    const lrot = lrad * (180 / Math.PI) * -1 + 180
+
+    const rx = rightEye.current.offsetLeft + rightEye.current.offsetWidth / 2
+    const ry = rightEye.current.offsetTop + rightEye.current.offsetHeight / 2
+    const rrad = Math.atan2(event.pageX - rx, event.pageY - ry)
+    const rrot = rrad * (180 / Math.PI) * -1 + 180
+
+    leftEye.current.style.transform = `rotate(${lrot}deg)`
+    rightEye.current.style.transform = `rotate(${rrot}deg)`
   }
 
   return (
@@ -14,8 +28,16 @@ export default function Error() {
       <Head>
         <title>Something went wrong</title>
       </Head>
-      <div style={style}>
-        <h1>Oops, something went wrong</h1>
+
+      <div className={styles.body} onMouseMove={moveEyes}>
+        <div>
+          <span className={styles.errorNum}>5</span>
+          <div ref={leftEye} className={styles.eye}></div>
+          <div ref={rightEye} className={styles.eye}></div>
+          <p className={styles.subText}>
+            Something went wrong. We&apos;re <i>looking</i> into what happened.
+          </p>
+        </div>
       </div>
     </>
   )
