33d401f8d9e911ce90f90c5ca4012cdc9d815dbf
Xander Warszawski <xander@xdoubleu.com>
2024-05-25 17:59:40 +0200

    Increase coverage web (#711)
    * Increase coverage web
    
    * Fix linter errors

--- web/src/layouts/ListViewLayout.tsx
+++ web/src/layouts/ListViewLayout.tsx
@@ -22,10 +22,6 @@ export interface List<T> {
   pagination: CustomPaginationProps
 }
 
-function isList<T>(list: List<T> | T[]): list is List<T> {
-  return "pagination" in list
-}
-
 interface ListViewLayoutProps<
   T extends { id: string | number },
   U extends List<T> | T[],
@@ -39,9 +35,7 @@ interface ListViewLayoutProps<
   form?: UseFormReturn<any>
   list: U
   setList: Dispatch<SetStateAction<U>>
-  apiCall: (
-    ...args: W
-  ) => Promise<APIResponse<WithRequired<List<X>, "data"> | X[]>>
+  apiCall: (...args: W) => Promise<APIResponse<WithRequired<List<X>, "data">>>
   apiCallArgs?: W
   preprocessList?: (data: WithRequired<List<X>, "data">) => Promise<U>
   createModal?: (props: ICreateModalProps<V>) => ReactNode
@@ -51,7 +45,7 @@ interface ListViewLayoutProps<
 // eslint-disable-next-line max-lines-per-function
 export default function ListViewLayout<
   T extends { id: string | number },
-  U extends List<T> | T[],
+  U extends List<T>,
   V extends FieldValues,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   W extends any[],
@@ -69,24 +63,19 @@ export default function ListViewLayout<
 }: ListViewLayoutProps<T, U, V, W, X>) {
   const router = useRouter()
 
-  const hasPagination = isList(list)
   const fetchData = useCallback(async () => {
     const page = router.query.page
       ? parseInt(router.query.page as string)
       : undefined
 
     const args = apiCallArgs ?? ([] as unknown as W)
-
-    if (hasPagination) {
-      args.push(page)
-    }
+    args.push(page)
 
     const response = await apiCall(...args)
 
     if (!response.data) return
 
     if (
-      isList(response.data) &&
       response.data.pagination.total !== 0 &&
       response.data.pagination.current > response.data.pagination.total
     ) {
@@ -97,12 +86,12 @@ export default function ListViewLayout<
     }
 
     const data = response.data
-    if (isList(data) && preprocessList) {
+    if (preprocessList) {
       setList(await preprocessList(data))
     } else {
       setList(data as unknown as U)
     }
-  }, [apiCall, apiCallArgs, preprocessList, setList, router, hasPagination])
+  }, [apiCall, apiCallArgs, preprocessList, setList, router])
 
   useEffect(() => {
     void fetchData()
@@ -116,40 +105,23 @@ export default function ListViewLayout<
       <br />
 
       <div className="min-vh-51">
-        {!((isList(list) && list.data) || list) && (
-          <Loader message="Fetching data." />
-        )}
+        {!list.data && <Loader message="Fetching data." />}
 
-        {(isList(list) && list.data?.length == 0) ||
-        (!isList(list) && list.length == 0)
-          ? "Nothing to see here."
-          : ""}
+        {list.data?.length == 0 ? "Nothing to see here." : ""}
 
-        {isList(list)
-          ? list.data?.map((item) => {
-              return (
-                <div key={item.id}>
-                  {card({ data: item, fetchData: fetchData })}
-                </div>
-              )
-            })
-          : list.map((item) => {
-              return (
-                <div key={item.id}>
-                  {card({ data: item, fetchData: fetchData })}
-                </div>
-              )
-            })}
+        {list.data?.map((item) => {
+          return (
+            <div key={item.id}>
+              {card({ data: item, fetchData: fetchData })}
+            </div>
+          )
+        })}
       </div>
 
-      {isList(list) ? (
-        <CustomPagination
-          current={list.pagination.current}
-          total={list.pagination.total}
-        />
-      ) : (
-        <></>
-      )}
+      <CustomPagination
+        current={list.pagination.current}
+        total={list.pagination.total}
+      />
     </ManagerLayout>
   )
 }
