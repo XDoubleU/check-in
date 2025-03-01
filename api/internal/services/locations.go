ce76332fae202e72b1f4a0727c991ced6183240f
Xander Warszawski <xander@xdoubleu.com>
2025-03-01 17:22:39 +0100

    Fix linter error

--- api/internal/services/locations.go
+++ api/internal/services/locations.go
@@ -65,7 +65,12 @@ func (service LocationService) GetCheckInsEntriesDay(
 	}
 
 	g := grapher.New[int](grapher.Cumulative, grapher.None, time.RFC3339, time.Second)
-	capacitiesGrapher := grapher.New[int](grapher.Normal, grapher.None, time.RFC3339, time.Second)
+	capacitiesGrapher := grapher.New[int](
+		grapher.Normal,
+		grapher.None,
+		time.RFC3339,
+		time.Second,
+	)
 
 	for _, checkIn := range checkIns {
 		datetime := timetools.LocationIndependentTime(checkIn.CreatedAt.Time, "UTC")
@@ -106,8 +111,18 @@ func (service LocationService) GetCheckInsEntriesRange(
 		return nil, nil, nil, err
 	}
 
-	g := grapher.New[int](grapher.CumulativeSameDate, grapher.None, time.RFC3339, time.Second)
-	capacitiesGrapher := grapher.New[int](grapher.Normal, grapher.None, time.RFC3339, time.Second)
+	g := grapher.New[int](
+		grapher.CumulativeSameDate,
+		grapher.None,
+		time.RFC3339,
+		time.Second,
+	)
+	capacitiesGrapher := grapher.New[int](
+		grapher.Normal,
+		grapher.None,
+		time.RFC3339,
+		time.Second,
+	)
 
 	for i := startDate; i.Before(endDate); i = i.AddDate(0, 0, 1) {
 		for _, schoolName := range schoolIDNameMap {
